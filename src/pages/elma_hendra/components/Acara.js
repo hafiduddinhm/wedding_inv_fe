e0ff58c8e9e0bdb96cf39b25c33cc6923d095f1a
fid3101 <hafiduddinmuhammad.hm@gmail.com>
2023-04-26 13:22:08 +0700

    add maps

--- src/pages/elma_hendra/components/Acara.js
+++ src/pages/elma_hendra/components/Acara.js
@@ -1,13 +1,30 @@
 import React, { forwardRef, useState, useEffect } from 'react';
-import { Grid, Box, Button, useTheme } from '@mui/material'
+import { Grid, Box, Button, useTheme, Typography } from '@mui/material'
+import { GoogleMap, LoadScript, MarkerF, InfoWindow } from '@react-google-maps/api';
 import PlaceIcon from '@mui/icons-material/PlaceOutlined';
 import backgroundImage from '../assets/image/bgAcara.png'
-// import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
 
 const Acara = forwardRef((props, sectionRef) => {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    
     const linkLokasi = 'https://goo.gl/maps/inffUEmwiEWBDq8YA'
+
+    const location = {
+        lat: -6.8771885,
+        lng: 111.5790249
+    };
+    const zoom = 17
+    const API_KEY = 'AIzaSyB1OuhrncfDYlFrz3iors1yZkO5z6VkH54'
+    const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${location.lat},${location.lng}&zoom=${location.zoom}`
+    const [map, setMap] = useState(null);
+
+    const onLoad = (map) => {
+        setMap(map);
+    }
+
+    const onUnmount = () => {
+        setMap(null);
+    }
+    
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     useEffect(() => {
         const handleResize = () => {
@@ -72,7 +89,7 @@ const Acara = forwardRef((props, sectionRef) => {
         <section ref={sectionRef} style={styles.section}>
             <Box sx={styles.box}>    
                 {/* <div style={styles.ornament1} /> */}
-                <Grid container spacing={4} style={{ position: 'relative', marginTop: windowWidth < 600 ? '60%' : `${20-windowWidth*0.01}%`, marginLeft: '-5%', marginBottom: '-20%', justifyContent: 'center' }}>
+                <Grid container spacing={4} style={{ position: 'relative', marginTop: windowWidth < 600 ? '60%' : `${20-windowWidth*0.01}%`, marginLeft: '-5%', justifyContent: 'center' }}>
                     <Grid data-aos='fade-right' data-aos-duration="1500"  item xs={12} sm={6} style={{justifyContent: 'center', padding: 0}}>
                         <h2 className="font-estetik" style={{textAlign: 'center'}}>Akad Nikah</h2>
                         <Box style={{ padding: windowWidth > 600 ? '1% 5% 5% 5%' : '1% 5% 5% 5%'}}>
@@ -86,15 +103,60 @@ const Acara = forwardRef((props, sectionRef) => {
                         </Box>
                     </Grid>
                 </Grid>
-                <Button data-aos='fade-up' data-aos-duration="1500" variant="contained" onClick={handleButtonClick} style={styles.btnStyles}>
+                <br/>
+                <div data-aos="fade-up" data-aos-duration="1500">
+                    <LoadScript
+                        googleMapsApiKey={API_KEY}
+                    >
+                        <GoogleMap
+                            mapContainerStyle={{width: '100%', height: '50vh'}}
+                            center={location}
+                            zoom={zoom}
+                            onLoad={onLoad}
+                            onUnmount={onUnmount}
+                            gestureHandling="cooperative"
+                            options={{ 
+                                zoomControlOptions: {
+                                    position: 9,
+                                    style: 3, // set the style of the zoom control to SMALL
+                                },
+                                mapTypeControl: false,
+                                streetViewControl: false,
+                            }}
+                        >
+                            <MarkerF 
+                                position={location} 
+                                // label={`Lokasi Resepsi`} 
+                                onClick={handleButtonClick}
+                            />
+                            <InfoWindow position={location} options={{disableAutoPan: true, closeBoxURL: ""}}>
+                                <div style={{display: 'flex',}}>
+                                    <PlaceIcon style={{ marginRight: "8px" }} />
+                                    <Typography onClick={handleButtonClick} variant="body1">Lokasi Resepsi</Typography>
+                                </div>
+                            </InfoWindow>
+                        </GoogleMap>
+                    </LoadScript>
+                </div>
+                
+                {/* <div style={{position: 'relative'}}>
+                    <iframe
+                        width="100%"
+                        height="140vh"
+                        src={mapUrl}
+                        allowFullScreen=""
+                        aria-hidden="false"
+                        tabIndex="0"
+                    />
+                </div> */}
+                {/* <Button data-aos='fade-up' data-aos-duration="1500" variant="contained" onClick={handleButtonClick} style={styles.btnStyles}>
                     <PlaceIcon style={{ marginRight: '7px', fontSize: '1rem' }} />
                     Lihat Lokasi
-                </Button>
-                <br/>
-                <br/>
+                </Button> */}
                 <br/>
                 <br/>
             </Box>
+            
         </section>
 
     );
